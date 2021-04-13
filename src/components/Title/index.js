6b34b5518cf0c4a3cefcb358cde0c5626171ee3a
pipi <pipi@gmail.com>
2021-04-13 13:42:16 +0800

    modify bug

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -3,7 +3,7 @@ import { useHistory } from 'react-router-dom'
 import styled from 'styled-components'
 
 import { Flex } from 'rebass'
-import Link from '../Link'
+// import Link from '../Link'
 import { RowFixed } from '../Row'
 // import Logo from '../../assets/logo.png'
 import Wordmark from '../../assets/logo.png'
